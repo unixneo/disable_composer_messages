dfb4423ac7c235213c94651e048c13081ffe99d1
neounix <neo@unix.com>
2021-03-09 09:44:58 +0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -17,17 +17,17 @@ after_initialize do
     requires_login
 
     def index
-      finder = ComposerMessagesFinder.new(current_user, params.slice(:composer_action, :topic_id, :post_id))
-      json = { composer_messages: [finder.find].compact }
-
-      if params[:topic_id].present?
-        topic = Topic.where(id: params[:topic_id]).first
-        if guardian.can_see?(topic)
-          json[:extras] = { duplicate_lookup: TopicLink.duplicate_lookup(topic) }
-        end
-      end
       if !Sitesetting.disable_composer_messages?
-        render_json_dump(json, rest_serializer: true)
+        finder = ComposerMessagesFinder.new(current_user, params.slice(:composer_action, :topic_id, :post_id))
+        json = { composer_messages: [finder.find].compact }
+
+        if params[:topic_id].present?
+          topic = Topic.where(id: params[:topic_id]).first
+          if guardian.can_see?(topic)
+            json[:extras] = { duplicate_lookup: TopicLink.duplicate_lookup(topic) }
+          end
+        end
+          render_json_dump(json, rest_serializer: true)
       end
     end
   end
